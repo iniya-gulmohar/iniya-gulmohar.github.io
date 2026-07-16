c8da421efe4e9f540cc752c46dcb1405c2bcf910
Irfan Jalal <mindhunter@blinkenshell.org>
2026-07-16 09:02:32 +0530

    added footer highlight for click

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,8 +2,14 @@ document.getElementById("about")?.addEventListener("click", function() {
   this.classList.toggle("expanded");
 });
 
-window.addEventListener("hashchange", () => {
-  if (window.location.hash === "#about") {
-    document.getElementById("about")?.classList.add("expanded");
-  }
+document.querySelector('a[href="#about"]')?.addEventListener("click", function() {
+  setTimeout(() => document.getElementById("about")?.classList.add("expanded"), 200);
+});
+
+document.querySelector('a[href="#footer"]')?.addEventListener("click", function() {
+  setTimeout(() => {
+    const el = document.getElementById("footer-copyright");
+    el?.classList.add("highlighted");
+    setTimeout(() => el?.classList.remove("highlighted"), 1500);
+  }, 200);
 });
